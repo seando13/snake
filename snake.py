3ad6ecfae410d8430e00bc06d82d36b524f3e467
Sean McCusker <sean.mccusker@gmail.com>
2019-03-28 13:05:32 +1100

    Added logic for adding snack cube to grid

--- snake.py
+++ snake.py
@@ -91,7 +91,20 @@ class snake(object):
         pass
 
     def addCube(self):
-        pass
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
 
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -123,7 +136,7 @@ def redrawWindow(surface):
 
 
 def randomSnack(rows, item):
-    global rows
+    
     positions = item.body
 
     while True:
